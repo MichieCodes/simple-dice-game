d2c8fd663a28b62aac5bd2c109e0e65aaf26e457
MichieTuts <michietuts@gmail.com>
2022-01-30 22:02:00 -0600

    Move form and related logic into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,18 @@
 import React from 'react'
-import {sleep} from './utils/sleep'
 
 import DiceContainer from './components/DiceContainer'
 import DiceHistory from './components/DiceHistory'
+import DiceRollForm from './components/DiceRollForm'
 
 import './App.scss'
 
 function App() {
-  const [numberOfDice, setNumberOfDice] = React.useState(1)
   const [dice, setDice] = React.useState([])
   const [loading, setLoading] = React.useState(false)
 
-  const randomizeDice = async (currentNumber, duration = 2000, delay = 100) => {
-    const ticks = duration / delay; 
-
-    for(let i = 0; i < ticks; i++) {
-      let randomDice = Array.from(
-        {length: currentNumber},
-        _ => 1 + Math.floor(Math.random() * 6)
-      )
-
-      setDice(randomDice)
-      await sleep(delay)
-    } 
-  }
-
-  const rollDice = async () => {
-    console.log("ROLLING: ", numberOfDice)
-
-    setLoading(true)
-
-    await randomizeDice(numberOfDice)
-
-    setLoading(false)
-  }
-
   return (
     <div className="App">
-      <h1>Number of Dice</h1>
-      
-      <div>
-        <input
-          type="number"
-          min="1"
-          value={numberOfDice}
-          onChange={(e) => setNumberOfDice(+e.target.value)}/>
-
-        <button onClick={rollDice}>
-          Roll
-        </button>
-      </div>
-
+      <DiceRollForm setDice={setDice} setLoading={setLoading}/> 
       <DiceContainer dice={dice}/>
       <DiceHistory lastDice={!loading && dice}/>
     </div>
---
+++ src/components/DiceRollForm/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import {sleep} from '/src/utils/sleep'
+
+function DiceRollForm({setDice, setLoading}) {
+  const [numberOfDice, setNumberOfDice] = React.useState(1)
+
+  const randomizeDice = async (
+    currentNumber,
+    duration = 2000,
+    delay = 100
+  ) => {
+    const ticks = duration / delay; 
+
+    for(let i = 0; i < ticks; i++) {
+      let randomDice = Array.from(
+        {length: currentNumber},
+        _ => 1 + Math.floor(Math.random() * 6)
+      )
+
+      setDice(randomDice)
+      await sleep(delay)
+    } 
+  }
+
+  const rollDice = async () => {
+    console.log("ROLLING: ", numberOfDice)
+
+    setLoading(true)
+
+    await randomizeDice(numberOfDice)
+
+    setLoading(false)
+  }
+
+  return (
+    <div>
+      <h1>Number of Dice</h1>
+
+      <input
+        type="number"
+        min="1"
+        value={numberOfDice}
+        onChange={(e) => setNumberOfDice(+e.target.value)}/>
+
+      <button onClick={rollDice}>
+        Roll
+      </button>
+    </div> 
+  )
+}
+
+export default DiceRollForm
